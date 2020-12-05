d51c88c102818f9032feca01ddde50b01f3a6f70
woku-wuzhe <wuzhe_@aliyun.com>
2020-12-05 11:35:05 +0800

    restful接口设计基础

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+// 处理跨域的请求
+const cors = require('@koa/cors')
+// 解析post请求体的中间件
+const koaBody = require('koa-body')
+// 美化json返回数据
+const json = require('koa-json')
 const app = new Koa()
 const router = new Router()
 
-router.get('/', ctx => {
-  console.log(ctx)
+// 给所有路由追加统一的前缀
+router.prefix('/api')
+
+router.get('/test', ctx => {
+  // 获取查询字符串
+  const params = ctx.request.query
+  console.log(params)
+  ctx.body = {
+    ...params
+  }
+})
+
+router.post('/post', async (ctx) => {
+  let { body } = ctx.request
   console.log(ctx.request)
-  ctx.body = 'Hello World!!'
+  ctx.body = {
+    ...body
+  }
 })
 
-router.get('/api', ctx => {
-  ctx.body = 'hello Api!!'
-})
-
-router.get('/async', async (ctx) => {
-  let result = await new Promise(resolve => {
-    setTimeout(function () {
-      resolve('hello world 2s later')
-    }, 1000)
-  })
-  ctx.body = result
-})
-
+app.use(koaBody())
+app.use(cors())
+app.use(json({
+  pretty: true
+}))
+// 中间件的顺序很重要
 app.use(router.routes()).use(router.allowedMethods())
 // 1. request, method, response是啥？
 // 2. api url => function, router路由的原理
