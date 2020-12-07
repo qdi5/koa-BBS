ca899cd0d4a33d2c84afd5ec0c0d8ddc4b145ab1
woku-wuzhe <wuzhe_@aliyun.com>
2020-12-07 15:27:29 +0800

    路由合并与静态资源服务

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const Koa = require('koa')
-const Router = require('koa-router')
+const path = require('path')
 // 处理跨域的请求
 const cors = require('@koa/cors')
 // 解析post请求体的中间件
 const koaBody = require('koa-body')
+// 安全头部
+const helmet = require('koa-helmet')
+// 静态资源服务
+const statics = require('koa-static')
 // 美化json返回数据
 const json = require('koa-json')
 const app = new Koa()
-const router = new Router()
-router.prefix('/api')
-
-
+// router.prefix('/api')
 const combineRouter = require('./routes/routes')
 
-
+app.use(helmet())
+app.use(statics(path.join(__dirname, 'public')))
 app.use(koaBody())
 app.use(cors())
 app.use(json({
   pretty: true
 }))
+
 // 中间件的顺序很重要
-app.use(router.routes()).use(router.allowedMethods())
+// app.use(router.routes()).use(router.allowedMethods())
 app.use(combineRouter())
 // 1. request, method, response是啥？
 // 2. api url => function, router路由的原理
