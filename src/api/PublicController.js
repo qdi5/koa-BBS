c52c2b9b67f264a674686d1cbf3c2364a8ea7d69
woku-wuzhe <wuzhe_@aliyun.com>
2020-12-10 10:25:54 +0800

    使用svgCaptcha库生成验证码

--- src/api/PublicController.js
+++ src/api/PublicController.js
@@ -1,9 +1,24 @@
+import svgCaptcha from 'svg-captcha'
+
 class PublicController {
   constructor () {}
   async getCaptcha (ctx) {
-    ctx.body = {
-      msg: '获取图形验证码'
+    const captcha = svgCaptcha.create({
+      size: 4,
+      width: 160,
+      height: 60,
+      fontSize: 50,
+      ignoreChars: '0oO1ilI',
+      noise: 4,
+      color: true,
+      background: '#eee'
+    })
+    console.log(captcha)
+    const data = {
+      data: captcha.data,
+      text: captcha.text
     }
+    ctx.body = captcha.data
   }
 }
 
